3e3649eaea411da3c4c79c839a7ae5ab2f7158fc
Kirana-P <kiranprakash226@gmail.com>
2026-07-28 22:41:44 +0530

    project filesadded

---
+++ src/main/java/aws2tier/Aws2tierApplication.java
@@ -0,0 +1,13 @@
+package aws2tier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Aws2tierApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Aws2tierApplication.class, args);
+	}
+
+}
---
+++ src/test/java/aws2tier/Aws2tierApplicationTests.java
@@ -0,0 +1,13 @@
+package aws2tier;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aws2tierApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
